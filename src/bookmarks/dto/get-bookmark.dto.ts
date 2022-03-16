83f1eac20beb4a7f8089c5840a3d0749c9c8f9d9
spieljs <spieljs@gmail.com>
2022-03-16 12:45:12 +0100

    implement get bookmarks by id and by userId

--- src/bookmarks/dto/get-bookmark.dto.ts
+++ src/bookmarks/dto/get-bookmark.dto.ts
@@ -1,7 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsDate } from 'class-validator';
+import { SearchHit } from '@elastic/elasticsearch/api/types';
+import { Bookmark } from '../bookmark.entity';
 
 export class GetBookmarkDto {
+    static fromSource(bookmarkSource: SearchHit<Bookmark>): GetBookmarkDto {
+        return new GetBookmarkDto(
+            bookmarkSource._source.id,
+            bookmarkSource._source.did,
+            bookmarkSource._source.userId,
+            bookmarkSource._source.description,
+            bookmarkSource._source.createdAt,
+        );
+    }
+
+
     @ApiProperty({
         example: 'b-123434',
         description: 'The identifier of the bookmark'
@@ -36,4 +49,18 @@ export class GetBookmarkDto {
     })
     @IsDate()
     createdAt: Date;
+
+    constructor(
+        id: string,
+        did: string,
+        userId: string,
+        description: string,
+        createdAt: Date,
+    ) {
+        this.id = id;
+        this.did = did;
+        this.userId = userId;
+        this.description = description;
+        this.createdAt= createdAt;
+    }
 }
