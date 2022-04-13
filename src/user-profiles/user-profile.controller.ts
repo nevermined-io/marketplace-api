3bab4f4259b185e6b96f5a7084974d9fdf8e4f22
spieljs <spieljs@gmail.com>
2022-04-13 15:53:21 +0200

    implement route to get user profile by userId

--- src/user-profiles/user-profile.controller.ts
+++ src/user-profiles/user-profile.controller.ts
@@ -1,4 +1,4 @@
-import { Post, Controller, Body } from '@nestjs/common';
+import { Post, Controller, Body, Get, Param } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserProfileService } from './user-profile.service';
 import { CreateUserProfileDto } from './dto/create-user-profile.dto';
@@ -24,5 +24,24 @@ export class UserProfileController {
   })
   async createUserProfile(@Body() createUserProfileDto: CreateUserProfileDto): Promise<GetUserProfileDto> {
     return this.userProfileService.createOne(createUserProfileDto);
+  }
+
+  @Get(':userId')
+  @ApiOperation({
+    description: 'Get the metadata of a user profile ',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'User profile returned',
+    type: GetUserProfileDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Not found',
+  })
+  async getUserProfileByUserId(@Param('userId') userId: string): Promise<GetUserProfileDto> {
+    const userProfileSource = await this.userProfileService.findOneById(userId);
+
+    return GetUserProfileDto.fromSource(userProfileSource);
   }
 }
