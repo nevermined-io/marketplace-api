9692e6439f5b16140a7a81e7898d87e46c97f57c
spieljs <spieljs@gmail.com>
2022-04-14 13:43:19 +0200

    fix incompatibilities in searching by text

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,11 @@ import { ConfigModule } from './shared/config/config.module';
 import { LoggerModule } from './shared/logger/logger.module';
 import { BookmarkModule } from './bookmarks/bookmark.module';
 import { AssetModule } from './assets/asset.module';
+import { UserProfileModule } from './user-profiles/user-profile.module';
 import { HttpsRedirectMiddleware } from './common/middlewares/https-redirection/https-redirection.middleware';
 
 @Module({
-  imports: [
-    RouterModule.forRoutes(routes),
-    AssetModule,
-    LoggerModule,
-    ConfigModule,
-    BookmarkModule,
-  ],
+  imports: [RouterModule.forRoutes(routes), AssetModule, LoggerModule, ConfigModule, BookmarkModule, UserProfileModule],
 })
 export class ApplicationModule {
   configure(consumer: MiddlewareConsumer) {
