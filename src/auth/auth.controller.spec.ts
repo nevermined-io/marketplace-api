2a4e8a8116827170f5e33f12a34d4a96facefce6
spieljs <spieljs@gmail.com>
2022-04-25 18:41:14 +0200

    implement create user profile in first login

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -8,44 +8,71 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from '../common/strategies/jwt.strategy';
 import { EthSignJWT } from '../common/guards/shared/jwt.utils';
+import { UserProfileService } from '../user-profiles/user-profile.service';
+import { UserProfile } from '../user-profiles/user-profile.entity';
+import { State, MarketplaceIndex } from '../common/type';
 
 describe('AuthController', () => {
-    let authController: AuthController;
-    let wallet: ethers.Wallet;
+  let authController: AuthController;
+  let wallet: ethers.Wallet;
 
-    beforeAll(() => {
-        wallet = ethers.Wallet.createRandom();
+  beforeAll(() => {
+    wallet = ethers.Wallet.createRandom();
+  });
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule,
+        PassportModule,
+        JwtModule.register({
+          secret: 'secret',
+          signOptions: { expiresIn: '60m' },
+        }),
+      ],
+      providers: [
+        AuthService,
+        JwtStrategy,
+        ConfigService,
+        {
+          provide: UserProfileService,
+          useValue: {
+            findOneByAddress: (address: string) => {
+              const userProfile = new UserProfile();
+              userProfile.addresses = [address];
+              userProfile.nickname = address;
+              userProfile.state = State.Confirmed;
+              userProfile.isListed = true;
+
+              return {
+                _source: userProfile,
+                _index: MarketplaceIndex.UserProfile,
+                _id: userProfile.userId,
+              };
+            },
+          },
+        },
+      ],
+      controllers: [AuthController],
+    }).compile();
+
+    authController = module.get<AuthController>(AuthController);
+  });
+
+  it('should get an access_token', async () => {
+    const clientAssertion = await new EthSignJWT({
+      iss: wallet.address,
+    })
+      .setProtectedHeader({ alg: 'ES256K' })
+      .setIssuedAt()
+      .setExpirationTime('60m')
+      .ethSign(wallet);
+    const clientAssertionType = 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer';
+
+    const response = await authController.login({
+      client_assertion_type: clientAssertionType,
+      client_assertion: clientAssertion,
     });
-
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            imports: [
-                ConfigModule,
-                PassportModule,
-                JwtModule.register({
-                    secret: 'secret',
-                    signOptions: { expiresIn: '60m' }
-                })
-            ],
-            providers: [AuthService, JwtStrategy, ConfigService],
-            controllers: [AuthController]
-        }).compile();
-
-        authController = module.get<AuthController>(AuthController);
-    });
-
-    it('should get an access_token', async () => {
-        const clientAssertion = await new EthSignJWT({
-            iss: wallet.address
-        })
-            .setProtectedHeader({ alg: 'ES256K' })
-            .setIssuedAt()
-            .setExpirationTime('60m')
-            .ethSign(wallet);
-        const clientAssertionType = 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer';
-
-        const response = authController.login(
-            { client_assertion_type: clientAssertionType, client_assertion: clientAssertion });
-        expect(response).toHaveProperty('access_token');
-    });
+    expect(response).toHaveProperty('access_token');
+  });
 });
