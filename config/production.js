7f33fb4f0a7bf98a0585a2f8c9f50d06dd1dab47
Rodolphe Marques <marques.rodolphe@gmail.com>
2023-10-12 15:10:19 +0200

    feat: updated passport-nevermined to v0.3.0
    - added new required env WEB3_PROVIDER_URI
    - requires on chain communication to validate logins done with smart
      contract wallets

--- config/production.js
+++ config/production.js
@@ -2,6 +2,7 @@ module.exports = {
   NODE_ENV: 'production',
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   JWT_EXPIRY_KEY: process.env.JWT_EXPIRY_KEY,
+  WEB3_PROVIDER_URI: process.env.WEB3_PROVIDER_URI,
   API_VERSION: process.env.API_VERSION,
   server: {
     port: process.env.PORT || 3100,
