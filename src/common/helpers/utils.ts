1fba50f73ba8927ba1cbd78c754690658f599ea0
Rodolphe Marques <marques.rodolphe@gmail.com>
2023-12-19 19:24:02 +0100

    fix: checkOwnership

--- src/common/helpers/utils.ts
+++ src/common/helpers/utils.ts
@@ -2,7 +2,7 @@ import { ForbiddenException } from '@nestjs/common'
 import { AuthRoles } from '../type'
 
 export const checkOwnership = (userId: string, entityUserId: string, roles: AuthRoles[]) => {
-  if (!roles.some((r) => r === AuthRoles.Admin) && userId !== entityUserId) {
+  if (!roles.some((r) => r === AuthRoles.Admin) || userId !== entityUserId) {
     throw new ForbiddenException('This source only can be created or updated by the owner or admin')
   }
 }
